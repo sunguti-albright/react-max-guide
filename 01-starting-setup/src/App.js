34c8c3639099fdc54ead72f51c3a19b5855b7973
sunguti-albright <sungutialbright@gmail.com>
2023-05-22 15:14:49 +0300

    add expenses component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -23,13 +23,19 @@ function App() {
     },
   ];
 
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <ExpenseItem title={expenses[0].title} date={expenses[0].date.toISOString()} amount={expenses[0].amount}></ExpenseItem>
+  //     <ExpenseItem title={expenses[1].title} date={expenses[1].date.toISOString()} amount={expenses[1].amount}></ExpenseItem>
+  //     <ExpenseItem title={expenses[2].title} date={expenses[2].date.toISOString()} amount={expenses[2].amount}></ExpenseItem>
+  //     <ExpenseItem title={expenses[3].title} date={expenses[3].date.toISOString()} amount={expenses[3].amount}></ExpenseItem>
+  //   </div>
+  // );
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} date={expenses[0].date.toISOString()} amount={expenses[0].amount}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} date={expenses[1].date.toISOString()} amount={expenses[1].amount}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} date={expenses[2].date.toISOString()} amount={expenses[2].amount}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} date={expenses[3].date.toISOString()} amount={expenses[3].amount}></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+
+  const clickHandler = () =>{
+    console.log('clicked!!')
+  }
+  
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>EVENT STATE</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
