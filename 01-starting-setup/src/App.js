efa4a189f816eb2e9e2c73d4f343a784df1b2293
sunguti-albright <sungutialbright@gmail.com>
2023-05-26 21:33:36 +0300

    code restructuring

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,29 +1,30 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
+function App() {
   //destructuring DUMMY EXPENSES to 'expenses'
 const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 //updating expenses based on previous expense snapshot...instead of initial expense state
@@ -32,6 +33,8 @@ const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
       return [expenseInfo, ...prevExpenses]
     })
   }
+
+
   // return (
   //   <div>
   //     <h2>Let's get started!</h2>
@@ -43,7 +46,7 @@ const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
   // );
   return (
     <div>
-  <NewExpense />
+  <NewExpense onAddExpenseData = {addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
