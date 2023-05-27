3ea10886da47429096b8a6872a3ff8b8ffb09b88
sunguti-albright <sungutialbright@gmail.com>
2023-05-27 21:35:51 +0300

    chore: use map() to display expense items dynamically

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
@@ -11,26 +13,16 @@ function Expenses(props) {
 
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      {props.items.map((expense) => (
+        <div key={expense.id}>
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        </div>
+      ))}
     </Card>
   );
 }
