5a3b75f2674a7e42f5512d6773a065598f102807
sunguti-albright <sungutialbright@gmail.com>
2023-06-01 18:25:56 +0300

    add dynamic chart styling to bar graph

--- 01-starting-setup/src/components/Chart/ChartBar.js
+++ 01-starting-setup/src/components/Chart/ChartBar.js
@@ -5,11 +5,12 @@ function ChartBar(props) {
   if(props.max > 0){
     barFillHeight = Math.round((props.value / props.maxValue * 100) + '%')
   }
-  
+
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill"></div>
+        <div className="chart-bar__fill" style={{height:barFillHeight, backgroundColor: 
+        'blue'}}></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
     </div>
---
+++ 01-starting-setup/src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function ExpensesChart(props) {
+    const chartDatapoints = [
+        {label:'Jan', value: 0},
+        {label:'Feb', value: 0},
+        {label:'Mar', value: 0},
+        {label:'Apr', value: 0},
+        {label:'May', value: 0},
+        {label:'Jun', value: 0},
+        {label:'Jul', value: 0},
+        {label:'Aug', value: 0},
+        {label:'Sep', value: 0},
+        {label:'Oct', value: 0},
+        {label:'Nov', value: 0},
+        {label:'Dec', value: 0},
+    ]
+
+    for (const expense in props.expenses) {
+        const expenseMonth = expense.date.getMonth(); //starting at [0] => January
+        chartDatapoints[]
+    }
+  return (
+   <Chart />
+  )
+}
+
+export default ExpensesChart
