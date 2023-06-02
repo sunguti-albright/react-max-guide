1410f7afb993b865a71ee1a4566c8e117d9a337b
sunguti-albright <sungutialbright@gmail.com>
2023-06-02 18:06:19 +0300

    calculate barchart height values in percentage

--- 01-starting-setup/src/components/Chart/ChartBar.js
+++ 01-starting-setup/src/components/Chart/ChartBar.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function ChartBar(props) {
+  let barFillHeight = "0%";
+  if(props.max > 0){
+    barFillHeight = Math.round((props.value / props.maxValue * 100) + '%')
+  }
+  
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
