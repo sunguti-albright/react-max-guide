09313a68296e73f6036636e87e8ccd1d197751c1
AlbrightSunguti <sungutialbright@gmail.com>
2023-06-04 09:50:49 +0300

    display expense values and maximum expense values in chart bar

--- 01-starting-setup/src/components/Chart/Chart.js
+++ 01-starting-setup/src/components/Chart/Chart.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import ChartBar from 'ChartBar'
+import ChartBar from './ChartBar'
 import './Chart.css'
 
 function Chart(props) {
+
    //getting the maximum expense value for selected month
    const dataPointsValues = props.dataPoints.map(datapoint => datapoint.value) //this returns an array(from the data object) with expense values only
    const totalMaximum = Math.max(...dataPointsValues) //return the maximum value for selected month, use spread operator to get values from array
@@ -12,10 +13,11 @@ function Chart(props) {
     props.dataPoints.map((datapoint)=>(
         <ChartBar
         // unique id/label ==> key
-        key={datapoint.key} 
+        key={datapoint.label} 
         value ={datapoint.value}
-        maxValue={null} 
-        label={datapoint.label}/>
+        maxValue={totalMaximum} 
+        label={datapoint.label}>
+        </ChartBar>
     ))
  }
 
