1de5df1502c773106c1225ab27f82dd45abb1953
AlbrightSunguti <sungutialbright@gmail.com>
2023-06-05 14:54:21 +0300

    use conditionals in styled components

--- 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,12 +2,46 @@ import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+import styled from 'styled-components';
 
 const CourseInput = props => {
+  const FormControl = styled.div`
+    margin: 0.5rem 0;
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color : ${props => (props.invalid ? 'red' : 'black')};
+  }
+  
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? 'ffd7d7' : '#fff')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+  
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+  
+  // &.invalid input {
+  //   background: #ffd7d7;
+  // }
+  
+  // &.invalid label {
+  //   color: #ffd7d7;
+  // }
+  `
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
   const goalInputChangeHandler = event => {
-    if(event.target.value.trim().length > 0){
+    if (event.target.value.trim().length > 0) {
       setIsValid(true)
     }
     setEnteredValue(event.target.value);
@@ -18,19 +52,20 @@ const CourseInput = props => {
     //check if user has inputted in the text field
     //alternatives: if enteredValue.length === 0 / enteredValue.triem().length === 0 / 
     if (enteredValue === "") {
-     setIsValid(false);
-     return
+      setIsValid(false);
+      return
     }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      {/* <FormControl className={ !isValid && 'invalid'} > */}
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} 
-                         />
-      </div>
+        <input type="text" onChange={goalInputChangeHandler}
+        />
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
