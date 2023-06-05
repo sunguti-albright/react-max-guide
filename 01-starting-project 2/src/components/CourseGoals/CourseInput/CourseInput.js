acb613456ece2e213cc942732d6319e7b81a0e57
AlbrightSunguti <sungutialbright@gmail.com>
2023-06-05 11:20:20 +0300

    useState to change styling when the input field in not empty

--- 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -7,6 +7,9 @@ const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -25,7 +28,12 @@ const CourseInput = props => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label style={{color: !isValid? 'red' : 'black'}}>Course Goal</label>
-        <input style={{borderColor: !isValid ? 'red' : 'black', backgroundColor: !isValid ? 'salmon' : 'transparent'}} type="text" onChange={goalInputChangeHandler} />
+        <input style={{
+                        borderColor: !isValid ? 'red' : 'black', 
+                        backgroundColor: !isValid ? 'salmon' : 'transparent'
+                        }} 
+                        type="text" onChange={goalInputChangeHandler} 
+                         />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
