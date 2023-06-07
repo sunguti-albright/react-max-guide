c391743b5c57eff950a4a7f902e14b6cc20f040d
AlbrightSunguti <sungutialbright@gmail.com>
2023-06-07 11:50:47 +0300

    chore:wrap content in auth context provider

--- side-effects/src/App.js
+++ side-effects/src/App.js
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-//useEffect to prevent infinite component looping && evaluation
-  useEffect(() => { 
+  //useEffect to prevent infinite component looping && evaluation
+  useEffect(() => {
     const loggedInStatus = localStorage.getItem("isLoggedIn");
     if (loggedInStatus === "yes") {
       setIsLoggedIn(true);
@@ -29,11 +30,13 @@ function App() {
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider value={{isLoggedIn : false}}>
+        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
