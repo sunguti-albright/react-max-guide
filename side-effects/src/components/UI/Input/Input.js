41346d69e06e8b799198dbbc48e92ff44d151459
AlbrightSunguti <sungutialbright@gmail.com>
2023-06-08 12:43:25 +0300

    userefs and imperative handle to pass refs

--- side-effects/src/components/UI/Input/Input.js
+++ side-effects/src/components/UI/Input/Input.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useRef, useImperativeHandle } from 'react'
 import classes from './Input.module.css'
 
-function Input(props) {
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef()
+
+  const activate =()  => {
+    inputRef.current.focus();
+
+  }
+
+//export functionalities to parent component (useImperativeHandler && forward refs)
+  useImperativeHandle(ref, ()=>{
+    return{
+      activate : activate
+    }
+  });
+
+  //this works too |||
+  // useEffect(() => {
+  //   inputRef.current.focus();
+  // }, []);
+  
   return (
     <div
-    className={`${classes.control} ${props.isValid === false ? classes.invalid : ''
-      }`}
-  >
-    <label htmlFor={props.id}>{props.label}</label>
-    <input
-      type={props.type}
-      id={props.id}
-      value={props.value}
-      onChange={props.onChange}
-      onBlur={props.onBlur}
-    />
-  </div>
+      className={`${classes.control} ${props.isValid === false ? classes.invalid : ''
+        }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
   )
-}
+});
 
 export default Input
